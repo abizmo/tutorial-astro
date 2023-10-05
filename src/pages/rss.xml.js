2ddce4dc7ec53ac2592b974675ae89d34c95c069
abizmo <abizmo@gmail.com>
2023-10-05 12:44:19 +0100

    migrate from file-based routing

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+import { getCollection } from 'astro:content'
 
-export async function GET(context) {
+export async function GET() {
+  const posts = await getCollection('posts')
+
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
-  });
+    site: 'https://example.com',
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+      pubDate: post.data.pubDate
+    })),
+    customData: `<language>en-us</language>`
+  })
 }
